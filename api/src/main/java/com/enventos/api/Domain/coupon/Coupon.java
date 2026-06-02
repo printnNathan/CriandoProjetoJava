5a4417990e4db30bf141c526abb78c831fbeb40e
Nathan <nathanferreira2222@gmail.com>
2026-06-01 22:14:07 -0300

    feat: creating entities

---
+++ api/src/main/java/com/enventos/api/Domain/coupon/Coupon.java
@@ -0,0 +1,31 @@
+package com.enventos.api.Domain.coupon;
+import com.enventos.api.Domain.event.Event;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Table(name = "coupon")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Coupon {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String code;
+    private Integer discount;
+    private Date valid;
+
+    @ManyToOne // um evento pode ter varios cupons mas um cupom pertence a um evento
+    @JoinColumn(name = "event_id")
+
+    private Event event;
+}
---
+++ api/src/main/java/com/enventos/api/Domain/event/Event.java
@@ -0,0 +1,28 @@
+package com.enventos.api.Domain.event;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.Date;
+import java.util.UUID;
+
+@Table(name = "event")
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Event {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+    private String title;
+    private String description;
+    private String imgUrl;
+    private String eventUrl;
+    private Boolean remote;
+    private Date date;
+
+}
